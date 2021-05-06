043caabe925c2345427cdf0a80c7a518cef5701d
Clint Siadis <clint-siadis@hotmail.com>
2021-05-07 00:05:03 +0200

    Add all tested people layout

---
+++ src/pages/testedPeople/single/index.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { Row, Col, Card, CardBody} from 'reactstrap';
+import classnames from 'classnames';
+
+import PageTitle from '../../../components/PageTitle';
+
+class singleLocation extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { activeTab: '2' };
+        this.toggle = this.toggle.bind(this);
+    }
+
+    /**
+     * Toggle the tab
+     */
+    toggle = (tab) => {
+        if (this.state.activeTab !== tab) {
+            this.setState({
+                activeTab: tab,
+            });
+        }
+    };
+
+    render() {
+        const reservationId = new URLSearchParams(window.location.search).get('id');
+
+        const tabContents = [
+            {
+                id: '1',
+                title: 'Normal',
+                icon: 'uil-home-alt',
+                text:
+                    'Home - Food truck quinoa dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.',
+            },
+            {
+                id: '2',
+                title: 'Urgent',
+                icon: 'uil-user',
+                text:
+                    'Profile - Food truck quinoa dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.',
+            },
+        ];
+
+        //const thisProduct = productsData.find(prod => prod.id === productId)
+
+        return (
+            <React.Fragment>
+                <Row className="page-title">
+                    <Col md={12}>
+                        <PageTitle
+                            breadCrumbItems={[{ label: 'Reservations', path: '/pages/starter', active: true }]}
+                            title={'Location info'}
+                        />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={12}>
+                        {/* tab pills */}
+                        <Card>
+                            <CardBody>
+                                <h5 className="header-title mb-3 mt-0">
+                                    This is where all the information will be. Just you wait. Reservation ID:{' '}
+                                    {reservationId}
+                                </h5>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+            </React.Fragment>
+        );
+    }
+}
+
+export default singleLocation;
--- src/routes/index.js
+++ src/routes/index.js
@@ -16,7 +16,7 @@ const Dashboard = React.lazy(() => import('../pages/dashboard'));
 const Locations = React.lazy(() => import('../pages/locations'));
 const AllReservations = React.lazy(() => import('../pages/reservations/all'));
 const TodayReservations = React.lazy(() => import('../pages/reservations/today'));
-const SingleReservation = React.lazy(() => import('../pages/reservations/single'));
+const AllTestedPeople = React.lazy(() => import('../pages/testedPeople'));
 // apps
 const CalendarApp = React.lazy(() => import('../pages/apps/Calendar'));
 const EmailInbox = React.lazy(() => import('../pages/apps/Email/Inbox'));
@@ -126,19 +126,19 @@ const reservationsAppRoutes = {
 };
 
 const testedPeopleAppRoutes = {
-    path: '/reservations',
+    path: '/tested-people',
     name: 'Tested',
     icon: FeatherIcon.Users,
     children: [
         {
-            path: '/reservations/all',
+            path: '/tested-people/all',
             name: 'All',
-            component: AllReservations,
+            component: AllTestedPeople,
             route: PrivateRoute,
             roles: ['Admin'],
         },
         {
-            path: '/reservations/today',
+            path: '/tested-people/today',
             name: 'Today',
             component: TodayReservations,
             route: PrivateRoute,
