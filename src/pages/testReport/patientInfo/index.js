462b070ed0974ee89f8614e671af4c3232b8ed07
Clint Siadis <clint-siadis@hotmail.com>
2021-05-12 03:15:13 +0200

    Add male icon for patient info

--- src/pages/testReport/patientInfo/index.js
+++ src/pages/testReport/patientInfo/index.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import { Card, CardBody, Row, Col, Progress } from 'reactstrap';
 
-import profileImg from '../../../assets/images/users/avatar-7.jpg';
-
 const PatientInfo = () => {
     return (
         <Card className="">
             <CardBody className="profile-user-box">
                 <Row>
                     <Col>
-                        <div className="text-center mt-3">
-                            <img src={profileImg} alt="" className="avatar-lg rounded-circle" />
+                        <div className="text-center mt-3 mb-4" style={{ lineHeight: '125px' }}>
+                            {/* <i className="uil uil-venus"></i> */}
+                            <i
+                                className="uil uil-mars"
+                                style={{
+                                    fontSize: '50px',
+                                    padding: 30,
+                                    borderRadius: '50%',
+                                    backgroundColor: 'gray',
+                                    color: 'white',
+                                }}></i>
                             <h5 className="mt-2 mb-0">John Doe</h5>
                         </div>
 
-                        <div className="mt-3 pt-2 border-top">
-                            <h4 className="mb-3 font-size-15">Personal Information</h4>
+                        <div className="pt-2 border-top">
+                            <h4 className="mb-2 mt-3 font-size-15 font-weight-bold">Personal Information</h4>
                             <div>
-                                <table className="table table-borderless mb-5 text-muted">
+                                <table className="table table-borderless text-muted">
                                     <tbody>
                                         <tr>
                                             <th scope="row">Firstname</th>
@@ -48,8 +55,8 @@ const PatientInfo = () => {
                             </div>
                         </div>
 
-                        <div className="mt-3 pt-2 border-top">
-                            <h4 className="mb-3 font-size-15">Contact Information</h4>
+                        <div className="pt-2 border-top">
+                            <h4 className="mb-2 mt-3 font-size-15 font-weight-bold">Contact Information</h4>
                             <div>
                                 <table className="table table-borderless mb-5 text-muted">
                                     <tbody>
