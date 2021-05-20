a3093e3419cb04f6fcb923c1aa6f5356d2832c86
Clint Siadis <clint-siadis@hotmail.com>
2021-05-20 02:07:19 +0200

    Strip location data

--- src/pages/locations/locations.jsx
+++ src/pages/locations/locations.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Row, Col, Card, CardBody, Table } from 'reactstrap';
 import axios, { useRequest } from '../../helpers/axios';
+// import { Link } from 'react-router-dom';
 import Loader from '../../components/Loader';
 import PageTitle from '../../components/PageTitle';
 
@@ -8,9 +9,36 @@ export default function Locations() {
     const data = useRequest('http://161.97.164.207/locations');
     if (!data) {
         return null;
-    }else{
-        console.log(data);
     }
+
+    function fetchSchedules(schedules) {
+        let finalSchedule = [];
+
+        for (const key in schedules) {
+            const times = '';
+            if (Object.hasOwnProperty.call(schedules, key)) {
+                const element = schedules[key];
+                const time = JSON.stringify(element, ['opening_time', 'closing_time']);
+                times = time;
+
+                // Object.entries(element).map(([key, val]) =>
+                //     finalSchedule.push(
+                //         <p key={key}>
+                //             {key}: {val}
+                //         </p>
+                //     )
+                // );
+            }
+            finalSchedule.push(
+                <p key={key}>
+                    {key} - {times}
+                </p>
+            );
+        }
+        return <div>{finalSchedule}</div>;
+    }
+
+    //console.log(schedule);
 
     return (
         <React.Fragment>
@@ -37,31 +65,31 @@ export default function Locations() {
                                         <tr>
                                             <th>#</th>
                                             <th>Name</th>
-                                            <th>Opening time</th>
-                                            <th>Closing time</th>
+                                            <th>Schedule</th>
                                             {/* <th>Address</th>
                                                 <th>Phone</th>
                                                 <th>Actions</th> */}
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        {/* {this.state.allLocations.map((record, index) => {
-                                                return (
-                                                    <tr key={index}>
-                                                        <th scope="row">{record.location_id}</th>
-                                                        <td>{record.location_name}</td>
-                                                        <td>{record.opening_time}</td>
-                                                        <td>{record.closing_time}</td>
-                                                        <td>
+                                        {data.map((record, index) => {
+                                            let schedules = record.schedule;
+
+                                            return (
+                                                <tr key={index}>
+                                                    <th scope="row">{record.location_id}</th>
+                                                    <td>{record.location_name}</td>
+                                                    <td>{fetchSchedules(schedules)}</td>
+                                                    {/* <td>
                                                             <Link to={`/view-location?id=` + record.id}>
                                                                 <Button color="primary" className="width-xs">
                                                                     View
                                                                 </Button>
                                                             </Link>
-                                                        </td>
-                                                    </tr>
-                                                );
-                                            })} */}
+                                                        </td> */}
+                                                </tr>
+                                            );
+                                        })}
                                     </tbody>
                                 </Table>
                             </CardBody>
