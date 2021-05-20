b963068c48773d35413150cea8afc4cab09a29a3
Clint Siadis <clint-siadis@hotmail.com>
2021-05-21 01:19:07 +0200

    Add function to fetch customer info

--- src/pages/testedPeople/today/index.jsx
+++ src/pages/testedPeople/today/index.jsx
@@ -7,8 +7,22 @@ import { Link } from 'react-router-dom';
 import BootstrapTable from 'react-bootstrap-table-next';
 import PageTitle from '../../../components/PageTitle';
 
+function FetchCustomerInfo({ customerId, type }) {
+    const customer = useRequest('http://161.97.164.207/customers/' + customerId);
+
+    if (!customer) {
+        return <span>Loading...</span>;
+    } else if (type == 'full_name') {
+        return customer.first_name + ' ' + customer.last_name;
+    } else if (type == 'date_of_birth') {
+        return customer.date_of_birth;
+    }else if (type == 'passport_number') {
+        return customer.passport_number;
+    }
+}
+
 export default function TestedPeople() {
-    const testedPeople = useRequest('https://run.mocky.io/v3/e44948a1-a757-4808-9bb6-b72bc24f790c');
+    const testedPeople = useRequest('http://161.97.164.207/reservations?offset=0&limit=20&tested=false');
     if (!testedPeople) {
         return null;
     }
@@ -16,16 +30,74 @@ export default function TestedPeople() {
     const { SearchBar } = Search;
     const { ExportCSVButton } = CSVExport;
 
+    addCustomProps(testedPeople);
+
+    function addCustomProps(testedPeople) {
+        // loop through list to add button
+        for (const person in testedPeople) {
+            if (Object.hasOwnProperty.call(testedPeople, person)) {
+                const record = testedPeople[person];
+                let testResult = 'Negative';
+                let resultTag = 'soft-danger';
+                let paidIcon = 'uil uil-money-withdrawal ml-2';
+                let paidIconTooltipTitle = 'cash';
+
+                // add custom item to array
+                record.actions = (
+                    <Link to={`/test-result?id=` + record.customer_id}>
+                        <Button color="primary" size="md">
+                            View
+                        </Button>
+                    </Link>
+                );
+
+                if (record.payment_method === 'pin') {
+                    paidIcon = 'uil uil-atm-card ml-2';
+                    paidIconTooltipTitle = 'pin';
+                }
+
+                record.full_name = <FetchCustomerInfo customerId={record.customer_id} type="full_name" />;
+                record.date_of_birth = <FetchCustomerInfo customerId={record.customer_id} type="date_of_birth" />;
+                record.passport_number = <FetchCustomerInfo customerId={record.customer_id} type="passport_number" />;
+
+                // record.paid_price_tag = (
+                //     <p>
+                //         {record.amount_paid}
+                //         <i className={paidIcon} id={'tooltip-' + record.reservation_id}></i>
+                //         <UncontrolledTooltip
+                //             placement="top"
+                //             id="tooltip-price-tooltip"
+                //             target={'tooltip-' + record.reservation_id}>
+                //             {record.payment_method}
+                //         </UncontrolledTooltip>
+                //     </p>
+                // );
+
+                // // check person result state to add tag styling
+                // if (record.test_result === true) {
+                //     testResult = 'Positive';
+                //     resultTag = 'soft-success';
+                // }
+
+                // record.result_tag = (
+                //     <Badge color={resultTag} className="mr-1">
+                //         {testResult}
+                //     </Badge>
+                // );
+            }
+        }
+    }
+
     const defaultSorted = [
         {
-            dataField: 'id',
+            dataField: 'reservation_id',
             order: 'asc',
         },
     ];
 
     const columns = [
         {
-            dataField: 'id',
+            dataField: 'reservation_id',
             text: 'ID',
             sort: true,
         },
@@ -35,13 +107,13 @@ export default function TestedPeople() {
             sort: true,
         },
         {
-            dataField: 'dob',
+            dataField: 'date_of_birth',
             text: 'Date of birth',
             sort: false,
         },
         {
-            dataField: 'address',
-            text: 'Address',
+            dataField: 'passport_number',
+            text: 'Paspoort',
             sort: true,
         },
         {
@@ -50,13 +122,8 @@ export default function TestedPeople() {
             sort: false,
         },
         {
-            dataField: 'paid_price_tag',
+            dataField: 'payment_method',
             text: 'Paid price',
-            sort: false,
-        },
-        {
-            dataField: 'result_tag',
-            text: 'Test result',
             sort: false,
         },
         {
@@ -87,7 +154,6 @@ export default function TestedPeople() {
     return (
         <React.Fragment>
             <div className="">
-
                 <Row className="page-title">
                     <Col md={12}>
                         <PageTitle
@@ -104,7 +170,7 @@ export default function TestedPeople() {
 
                                 <ToolkitProvider
                                     bootstrap4
-                                    keyField="id"
+                                    keyField="reservation_id"
                                     data={testedPeople}
                                     columns={columns}
                                     search
