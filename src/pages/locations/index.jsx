2ab5c5b1908e8fb22753eb0749478c73b37ce7c7
Clint Siadis <clint-siadis@hotmail.com>
2021-05-23 01:58:23 +0200

    Add schedules in table with opening and closing time

--- src/pages/locations/index.jsx
+++ src/pages/locations/index.jsx
@@ -1,102 +1,194 @@
 import React from 'react';
-import { Row, Col, Card, CardBody, Table } from 'reactstrap';
-import axios, { useRequest } from '../../helpers/axios';
-// import { Link } from 'react-router-dom';
-import Loader from '../../components/Loader';
+import { Row, Col, Card, CardBody, Input, Button } from 'reactstrap';
+import { useRequest } from '../../helpers/axios';
+import BootstrapTable from 'react-bootstrap-table-next';
+import { Link } from 'react-router-dom';
+import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit';
+import paginationFactory from 'react-bootstrap-table2-paginator';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import PageTitle from '../../components/PageTitle';
 
-export default function Locations() {
-    const data = useRequest('http://161.97.164.207/locations');
-    if (!data) {
-        return null;
+const sizePerPageRenderer = ({ options, currSizePerPage, onSizePerPageChange }) => (
+    <React.Fragment>
+        <label className="d-inline mr-1">Show</label>
+        <Input
+            type="select"
+            name="select"
+            id="no-entries"
+            className="custom-select custom-select-sm d-inline col-1"
+            defaultValue={currSizePerPage}
+            onChange={(e) => onSizePerPageChange(e.target.value)}>
+            {options.map((option, idx) => {
+                return <option key={idx}>{option.text}</option>;
+            })}
+        </Input>
+        <label className="d-inline ml-1">entries</label>
+    </React.Fragment>
+);
+
+function FetchCustomerInfo({ customerId, type }) {
+    const customer = useRequest('http://161.97.164.207/customers/' + customerId);
+
+    if (!customer) {
+        return <span>Loading...</span>;
+    } else if (type === 'first_name') {
+        return customer.first_name;
+    } else if (type === 'date_of_birth') {
+        return customer.date_of_birth;
+    } else if (type === 'last_name') {
+        return customer.last_name;
+    } else if (type === 'passport_number') {
+        return customer.passport_number;
     }
+}
 
-    function fetchSchedules(schedules) {
-        let finalSchedule = [];
+function scheduleFormatter(cell, row, rowIndex) {
+    let schedule = [];
+    let openingTime = '';
+    let closingTime = '';
 
-        for (const key in schedules) {
-            const times = '';
-            if (Object.hasOwnProperty.call(schedules, key)) {
-                const element = schedules[key];
-                const time = JSON.stringify(element, ['opening_time', 'closing_time']);
-                times = time;
+    for (const day in row.schedule) {
+        if (Object.hasOwnProperty.call(row.schedule, day)) {
+            const timesRaw = row.schedule[day];
 
-                // Object.entries(element).map(([key, val]) =>
-                //     finalSchedule.push(
-                //         <p key={key}>
-                //             {key}: {val}
-                //         </p>
-                //     )
-                // );
+            if (timesRaw) {
+                openingTime = timesRaw.opening_time;
+                closingTime = timesRaw.closing_time;
+            } else {
+                openingTime = '';
+                closingTime = '';
             }
-            finalSchedule.push(
-                <p key={key}>
-                    {key} - {times}
-                </p>
-            );
         }
-        return <div>{finalSchedule}</div>;
+
+        schedule.push(
+            <div className="text-left text-capitalize mb-2">
+                <span>{day}</span>
+                <span style={{ float: 'right' }}>
+                    {openingTime} - {closingTime}
+                </span>
+            </div>
+        );
     }
 
-    //console.log(schedule);
+    return schedule;
+}
 
+function addActionsButton(cell, row, rowIndex) {
     return (
-        <React.Fragment>
-            <div className="">
-                {/* preloader */}
-                {/* {this.props.loading && <Loader />} */}
-
-                <Row className="page-title">
-                    <Col md={12}>
-                        <PageTitle
-                            breadCrumbItems={[{ label: 'Locations', path: '/pages/starter', active: true }]}
-                            title={'All Locations'}
-                        />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md={12}>
-                        <Card>
-                            <CardBody>
-                                <p className="sub-header">Here you will find all the Carephar locations</p>
-
-                                <Table className="mb-0" hover>
-                                    <thead>
-                                        <tr>
-                                            <th>#</th>
-                                            <th>Name</th>
-                                            <th>Schedule</th>
-                                            {/* <th>Address</th>
-                                                <th>Phone</th>
-                                                <th>Actions</th> */}
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {data.map((record, index) => {
-                                            let schedules = record.schedule;
-
-                                            return (
-                                                <tr key={index}>
-                                                    <th scope="row">{record.location_id}</th>
-                                                    <td>{record.location_name}</td>
-                                                    <td>{fetchSchedules(schedules)}</td>
-                                                    {/* <td>
-                                                            <Link to={`/view-location?id=` + record.id}>
-                                                                <Button color="primary" className="width-xs">
-                                                                    View
-                                                                </Button>
-                                                            </Link>
-                                                        </td> */}
-                                                </tr>
-                                            );
-                                        })}
-                                    </tbody>
-                                </Table>
-                            </CardBody>
-                        </Card>
-                    </Col>
-                </Row>
-            </div>
-        </React.Fragment>
+        <div className="text-left">
+            <Link to={`/test-result/` + row.location_id}>
+                <Button color="primary" size="md">
+                    View
+                </Button>
+            </Link>
+        </div>
     );
 }
+
+const columns = [
+    {
+        dataField: 'location_id',
+        text: '#',
+        sort: true,
+    },
+    {
+        dataField: 'location_name',
+        text: 'Location name',
+        sort: true,
+    },
+    {
+        dataField: 'reservation_datetime',
+        text: 'Schedule',
+        sort: false,
+        formatter: scheduleFormatter,
+    },
+    {
+        dataField: 'actions',
+        text: 'Actions',
+        sort: false,
+        formatter: addActionsButton,
+    },
+];
+
+const defaultSorted = [
+    {
+        dataField: 'location_id',
+        order: 'asc',
+    },
+];
+
+const Table = () => {
+    const locations = useRequest('http://161.97.164.207/locations');
+    if (!locations) {
+        return null;
+    }
+    const { SearchBar } = Search;
+
+    return (
+        <Card>
+            <CardBody>
+                <p className="sub-header">Here you can find all the locations</p>
+                <ToolkitProvider
+                    bootstrap4
+                    keyField="location_id"
+                    data={locations}
+                    columns={columns}
+                    search={{
+                        searchFormatted: true,
+                    }}
+                    columnToggle>
+                    {(props) => (
+                        <React.Fragment>
+                            <Row>
+                                <Col className="text-right">
+                                    <SearchBar {...props.searchProps} />
+                                </Col>
+                            </Row>
+
+                            <BootstrapTable
+                                {...props.baseProps}
+                                bordered={false}
+                                //striped
+                                hover
+                                defaultSorted={defaultSorted}
+                                pagination={paginationFactory({
+                                    sizePerPage: 10,
+                                    sizePerPageRenderer: sizePerPageRenderer,
+                                    sizePerPageList: [
+                                        { text: '10', value: 10 },
+                                        { text: '50', value: 50 },
+                                        { text: '100', value: 100 },
+                                        { text: 'All', value: locations.length },
+                                    ],
+                                })}
+                                wrapperClasses="table-responsive"
+                            />
+                        </React.Fragment>
+                    )}
+                </ToolkitProvider>
+            </CardBody>
+        </Card>
+    );
+};
+
+const AllLocations = () => {
+    return (
+        <React.Fragment>
+            <Row className="page-title">
+                <Col md={12}>
+                    <PageTitle
+                        breadCrumbItems={[{ label: 'All locations', path: '/pages/starter', active: true }]}
+                        title={'All Locations'}
+                    />
+                </Col>
+            </Row>
+            <Row>
+                <Col md={12}>
+                    <Table />
+                </Col>
+            </Row>
+        </React.Fragment>
+    );
+};
+
+export default AllLocations;
