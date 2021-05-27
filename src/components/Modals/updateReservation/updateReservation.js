9d5290950d331d5b3b46d44beb0b3c83ceac4aa6
Clint Siadis <clint-siadis@hotmail.com>
2021-05-27 23:48:47 +0200

    Add wizard steps in modal

--- src/components/Modals/updateReservation/updateReservation.js
+++ src/components/Modals/updateReservation/updateReservation.js
@@ -1,26 +1,94 @@
 import React from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import {
+    Button,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+    Progress,
+    FormGroup,
+    CustomInput,
+    Row,
+    Col,
+} from 'reactstrap';
+import { AvForm, AvField, AvInput } from 'availity-reactstrap-validation';
+import { Wizard, Steps, Step } from 'react-albus';
 
 const updateReservationModal = ({ modalState, toggle, reservationdId }) => {
     console.log(reservationdId);
     return (
         <Modal isOpen={modalState} toggle={toggle}>
-            <ModalHeader toggle={toggle}>updateReservationModal {reservationdId}</ModalHeader>
+            <ModalHeader toggle={toggle}>Reservation</ModalHeader>
             <ModalBody>
-                <h6>Text in a modal</h6>
-                <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
-                <hr />
-                <h6>Overflowing text to show scroll behavior</h6>
-                <p>
-                    Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas
-                    eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-                </p>
+                <Wizard
+                    render={({ step, steps }) => (
+                        <React.Fragment>
+                            <Progress
+                                animated
+                                striped
+                                color="success"
+                                value={((steps.indexOf(step) + 1) / steps.length) * 100}
+                                className="mb-3 progress-sm"
+                            />
+
+                            <Steps>
+                                <Step
+                                    id="AddTestTube"
+                                    render={({ next }) => (
+                                        <AvForm
+                                            onValidSubmit={(event, values) => {
+                                                next();
+                                            }}>
+                                            <AvField name="username" label="Test tube id" type="text" required />
+                                            {/* <AvField name="password" label="Password" type="password" required /> */}
+                                            
+                                            <ul className="list-inline wizard mb-0">
+                                                <li className="next list-inline-item float-right">
+                                                    <Button color="success" type="submit">
+                                                        Next
+                                                    </Button>
+                                                </li>
+                                            </ul>
+                                        </AvForm>
+                                    )}
+                                />
+                                <Step
+                                    id="AddPayment"
+                                    render={({ next, previous }) => (
+                                        <AvForm
+                                            onValidSubmit={(event, values) => {
+                                                next();
+                                            }}>
+                                            <AvField name="payment_method" label="Payment method" type="text" required />
+                                            <AvField name="padi_amount" label="Paid amount" type="number" required />
+
+
+                                            <ul className="list-inline wizard mb-0">
+                                                <li className="previous list-inline-item">
+                                                    <Button onClick={previous} color="light">
+                                                        Previous
+                                                    </Button>
+                                                </li>
+                                                <li className="next list-inline-item float-right">
+                                                    <Button color="success" type="submit">
+                                                        Next
+                                                    </Button>
+                                                </li>
+                                            </ul>
+                                        </AvForm>
+                                    )}
+                                />
+                                
+                            </Steps>
+                        </React.Fragment>
+                    )}
+                />
             </ModalBody>
-            <ModalFooter>
+            {/* <ModalFooter>
                 <Button color="secondary" className="ml-1" onClick={toggle}>
                     Cancel
                 </Button>
-            </ModalFooter>
+            </ModalFooter> */}
         </Modal>
     );
 };
